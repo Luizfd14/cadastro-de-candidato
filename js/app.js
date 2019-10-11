828fe975effda647ac0303d8ea781fd494d05f73
luiz_douglas_f@hotmail.com <pdl@PCTI05.palaciodosleiloes.local>
2019-10-11 14:20:36 -0300

    Tirado a caixa confirmada

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/**
+ * CONFIGURACOES DO FIREBASE
+ */
 var firebaseConfig = {
   apiKey: "AIzaSyDyRLghpOVv6eEqz37Fo1fEe6Wk53qZCFE",
   authDomain: "palacio-1546964719574.firebaseapp.com",
@@ -8,22 +11,34 @@ var firebaseConfig = {
   appId: "1:561892950747:web:454ef6ed5d85f8fcce82fe"
 };
 
+//  - INICIALIZA O FIREBASE
 firebase.initializeApp(firebaseConfig);
 
-//data
-var data = new Date();
-var dia = data.getDate(); // 1-31
-var mes = data.getMonth(); // 0-11 (zero=janeiro)
-var ano2 = data.getFullYear(); // 2 dígitos
-var dataHJ = dia + "_" + mes + "_" + ano2;
 
-const dbRef = firebase.database().ref();
-const testesRef = dbRef.child("recrutamento/" + dataHJ);
+const db = firebase.database().ref();  //  - INSTANCIA DO BANCO
+const teste = db.child("recrutamento/" + dataHJ); // DEFINE O ROOT
 
+// ACAO QUANDO UM NO VO REGISTRO E ADICIONADO
 testesRef.on("child_added", snap => {
   M.toast({ html: "Novo teste liberado!" });
 });
 
+/**
+ * REGISTRAR UM NOVO TESTE
+ * 
+ * REGISTRA UM NOVO TESTE NO SISTEMA
+ */
+function registrarTeste(){
+  
+}
+
+/**
+ * GET HORA AGORA
+ * 
+ * RETORNA HORA NO MOMENTO DO GET
+ * 
+ * @return tempo
+ */
 function getHoraAgora() {
   let tempo = new Date();
   var hora = data.getHours(); // 0-23
@@ -31,6 +46,22 @@ function getHoraAgora() {
   var seg = data.getSeconds(); // 0-59
   tempo = hora + ":" + min + ":" + seg;
   return tempo;
+}
+
+/**
+ * GET DIA MES E ANO
+ * 
+ * RETORNA A DATA NO FORMATO DD_MM_AAAA
+ * 
+ * @return data
+ */
+function getDiaMesAno(){
+  var data = new Date();
+  var dia = data.getDate(); // 1-31
+  var mes = data.getMonth(); // 0-11 (zero=janeiro)
+  var ano2 = data.getFullYear(); // 2 dígitos
+  var dataHJ = dia + "_" + mes + "_" + ano2;
+  return dataHJ
 }
 
 function gravar(nome, word, excel, cadastro, tempo, hrInicio, hrFinal) {
